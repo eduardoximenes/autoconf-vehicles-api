de8d4b038296f7690cf3c4a445fe5e5de7543321
Eduardo Ximenes <duduximenes470@gmail.com>
2025-09-13 15:20:43 -0300

    feat: configura rotas da API

---
+++ routes/api.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\V1\AuthController;
+
+Route::prefix('v1')->group(function () {
+    Route::middleware('throttle:3,1')->group(function () {
+        Route::post('/auth/register', [AuthController::class, 'register']);
+        Route::post('/auth/login', [AuthController::class, 'login']);
+    });
+
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::post('/auth/logout', [AuthController::class, 'logout']);
+        Route::get('/auth/me', [AuthController::class, 'me']);
+    });
+});
