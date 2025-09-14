5420859cba32bc7f672a6f2670d21ab231be8d62
Eduardo Ximenes <duduximenes470@gmail.com>
2025-09-14 19:09:58 -0300

    feat: configura rotas de Vehicle/VehicleImage na API

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\V1\AuthController;
+use App\Http\Controllers\Api\V1\VehicleController;
+use App\Http\Controllers\Api\V1\VehicleImageController;
 
 Route::prefix('v1')->group(function () {
     Route::middleware('throttle:3,1')->group(function () {
@@ -13,5 +15,11 @@ Route::prefix('v1')->group(function () {
     Route::middleware('auth:sanctum')->group(function () {
         Route::post('/auth/logout', [AuthController::class, 'logout']);
         Route::get('/auth/me', [AuthController::class, 'me']);
+
+        Route::apiResource('vehicles', VehicleController::class);
+
+        Route::post('/vehicles/{vehicleId}/images', [VehicleImageController::class, 'store']);
+        Route::patch('/vehicles/{vehicleId}/images/{imageId}/cover', [VehicleImageController::class, 'setCover']);
+        Route::delete('/vehicles/{vehicleId}/images/{imageId}', [VehicleImageController::class, 'destroy']);
     });
 });
